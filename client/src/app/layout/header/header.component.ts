27ad9c6c38273c4bd9f16ba0c8da01b0f9483118
Senthil-Code28 <senthil.k1212@gmail.com>
2025-01-02 00:20:58 -0500

    End of section 10

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 
@@ -12,7 +13,9 @@ import { MatIcon } from '@angular/material/icon';
   imports: [
    MatIcon,
    MatButton,
-   MatBadge
+   MatBadge,
+   RouterLink,
+   RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
