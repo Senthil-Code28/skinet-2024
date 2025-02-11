c4a73349228810a7107527679c07d56f5d206403
Senthil-Code28 <senthil.k1212@gmail.com>
2025-02-11 01:39:36 -0500

    End of section 18

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51QlZpvDmL3zENc0IX4fopFsleKWd6ZwfRUiaan9IFxzGo9Bqx0n9YZAUtbCT2ZlEGO3LDgBxe6eTWE91GrReUpMh00RGn6XEbH'
 };
